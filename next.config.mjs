5351994bc67c163eb01557fb38cebdd69c3eab07
Victor  Gonçalves <vbhy91zr@duck.com>
2026-06-01 19:05:59 -0300

    fix: allow ceusgame.com in next.config.mjs for Next.js image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ceusgame.com',
+        port: '5522',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ceusgame.com',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5522',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+};
 export default nextConfig;
